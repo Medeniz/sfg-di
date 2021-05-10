6b2e68258161a235334b0721e8ef6830e49acf2f
medeniz <medeniz@gmail.com>
2021-05-10 15:44:26 +0200

    Qualifiers

--- src/main/java/guru/springframework/di/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/di/controllers/ConstructorInjectedController.java
@@ -1,12 +1,15 @@
 package guru.springframework.di.controllers;
 
 import guru.springframework.di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/main/java/guru/springframework/di/services/GreetingServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.di.services;
-
-public class GreetingServiceImpl implements GreetingService{
-
-    @Override
-    public String sayGreeting() {
-        return "Hello World";
-    }
-}
